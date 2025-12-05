eb64a904b00294d090a5390acbb514d7d8b3a9a5
Ruben <180013@iessanalberto.com>
2025-12-05 09:18:40 +0100

    ff

--- ciudadano.py
+++ ciudadano.py
@@ -8,3 +8,14 @@ class Ciudadano:
         self.size = 20
         self.color = (0, 150, 255)
 
+    def mover(self, keys):
+        personaje = (self.x, self.y)
+        if keys [pygame.K_LEFT]: self.x -= self.vel
+        if keys [pygame.K_RIGHT]: self.x += self.vel
+        if keys [pygame.K_UP]: self.y += self.vel
+        if keys [pygame.K_DOWN]: self.y -= self.vel
+        return (self.x, self.y)
+
+    def dibujar(self,win):
+        pygame.draw.rect(win, self.color,(self.x, self.y, self.size))
+
--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-#Empezar con los imports
+import pygame
+import sys
+from ciudadano import Ciudadano
+
+pygame.init()
+win = pygame.display.set_mode((800, 800))
+
+jugador = Ciudadano(100,100)
+tiendas = [
+    pygame.Rect(50, 150, 60, 80),
+    pygame.Rect(150, 150, 60, 80),
+    pygame.Rect(250, 150, 60, 80),
+    pygame.Rect(350, 150, 60, 80),
+    pygame.Rect(450, 150, 60, 80)
+]
+colores = [
+    pygame.color
+]
+
+run = True
+while run:
+    for e in pygame.event.get():
+        if e.type = pygame.QUIT:
+            run = False
+        keys = pygame.key.get_pressed()
+        quieto = jugador.mover(keys)
+        tienda_actual = -1
+        if quieto:
+            for i,t in enumerate(tiendas):
+
