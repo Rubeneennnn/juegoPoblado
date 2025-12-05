5c4e5558648808e0d51dd061d5312b69782c6f49
Rubeneennnn <rubeninigueztorron@gmail.com>
2025-12-05 08:00:32 +0100

    saaa

---
+++ ciudadano.py
@@ -0,0 +1,10 @@
+import pygame
+
+class Ciudadano:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.vel = 4
+        self.size = 20
+        self.color = (0, 150, 255)
+
