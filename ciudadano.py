a011ff3ad75987f0ae4891b0ed5e57aa634ee1c1
Rubeneennnn <rubeninigueztorron@gmail.com>
2025-12-06 15:22:10 +0100

    rr

--- ciudadano.py
+++ ciudadano.py
@@ -4,7 +4,7 @@ class Ciudadano:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.vel = 4
+        self.vel = 1
         self.size = 20
         self.color = (0, 150, 255)
 
@@ -12,10 +12,10 @@ class Ciudadano:
         personaje = (self.x, self.y)
         if keys [pygame.K_LEFT]: self.x -= self.vel
         if keys [pygame.K_RIGHT]: self.x += self.vel
-        if keys [pygame.K_UP]: self.y += self.vel
-        if keys [pygame.K_DOWN]: self.y -= self.vel
+        if keys [pygame.K_UP]: self.y -= self.vel
+        if keys [pygame.K_DOWN]: self.y += self.vel
         return (self.x, self.y)
 
     def dibujar(self,win):
-        pygame.draw.rect(win, self.color,(self.x, self.y, self.size))
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.size, self.size))
 
--- main.py
+++ main.py
@@ -4,8 +4,12 @@ from ciudadano import Ciudadano
 
 pygame.init()
 win = pygame.display.set_mode((800, 800))
+clock = pygame.time.Clock()
+
 
 jugador = Ciudadano(100,100)
+color_fondo_normal = (230, 230, 230)
+
 tiendas = [
     pygame.Rect(50, 150, 60, 80),
     pygame.Rect(150, 150, 60, 80),
@@ -13,18 +17,39 @@ tiendas = [
     pygame.Rect(350, 150, 60, 80),
     pygame.Rect(450, 150, 60, 80)
 ]
-colores = [
-    pygame.color
+fondos = [
+    (200, 100, 100),
+    (100, 200, 100),
+    (100, 100, 200),
+    (200, 200, 100),
+    (200, 100, 200)
 ]
 
 run = True
 while run:
-    for e in pygame.event.get():
-        if e.type = pygame.QUIT:
-            run = False
-        keys = pygame.key.get_pressed()
-        quieto = jugador.mover(keys)
-        tienda_actual = -1
-        if quieto:
-            for i,t in enumerate(tiendas):
+    clock.tick(60)
 
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            run = False
+
+    keys = pygame.key.get_pressed()
+    quieto = jugador.mover(keys)
+
+    tienda_actual = -1
+    if quieto:
+        for i,t in enumerate(tiendas):
+            if t.collidepoint(jugador.x + jugador.size//2, jugador.y + jugador.size//2):
+                tienda_actual = i
+
+    if tienda_actual >= 0:
+        win.fill(fondos[tienda_actual])
+    else:
+        win.fill(color_fondo_normal)
+        for t in tiendas:
+            pygame.draw.rect(win, (120,120,120), t)
+
+    jugador.dibujar(win)
+    pygame.display.update()
+
+pygame.quit()
